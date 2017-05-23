b98b804ea83723d476f50a46e9ba4dbe8fc6e4a4
Tomasz Rogozik <trogozik@applause.com>
2017-05-23 11:46:02 +0200

    Add cffi to the package requirements.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='kms-encryption-toolbox',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/ApplauseOSS/kms-encryption-toolbox',
     license='Applause',
     description='Encryption toolbox to be used with the Amazon Key Management Service for securing your deployment secrets. It encapsulates the aws-encryption-sdk package to expose cmdline actions.',
@@ -12,6 +12,7 @@ setup(
     zip_safe=False,
     packages=find_packages(),
     install_requires=[
+        'cffi>=1.10.0',
         'aws-encryption-sdk>=1.2.0',
         'click>=6.6',
         'attrs>=16.3.0,<17.0.0'
