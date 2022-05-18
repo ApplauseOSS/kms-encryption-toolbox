f421878f41e51b621d31c797f06d8338160b30fa
Andrew Gaffney <agaffney@applause.com>
2022-05-18 20:32:28 +0000

    Upgrade aws-encryption-sdk to work with newer cryptography

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ class ReleaseToPyPICommand(upload):
 
 setup(
     name='kms-encryption-toolbox',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/ApplauseOSS/kms-encryption-toolbox',
     license='MIT',
     description='Encryption toolbox to be used with the Amazon Key Management Service for securing your deployment secrets. It encapsulates the aws-encryption-sdk package to expose cmdline actions.',
@@ -22,7 +22,7 @@ setup(
     packages=['kmsencryption'],
     install_requires=[
         'cffi>=1.10.0',
-        'aws-encryption-sdk>=1.2.0,<2.0',
+        'aws-encryption-sdk>=3,<4',
         'click>=6.6',
         'cryptography>=1.8.1,!=3.4',
         'future>=0.16.0'
